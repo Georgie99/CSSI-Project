71b2bbbaa7baf03ad89c563f084ad4556784031b
Shruthi <iamshruthiiyer@gmail.com>
2018-07-23 15:06:06 -0400

    all filters complete!

--- main.py
+++ main.py
@@ -22,34 +22,13 @@ class PreferencePage(webapp2.RequestHandler):
         pref = self.request.get("pref")
         color = self.request.get("color")
         character_query = Character.query()
-        if(skill=="beginner"):
-            characters = character_query.filter(Character.skill==3).fetch()
-        elif(skill=="medium"):
-            characters = character_query.filter(Character.skill==2).fetch()
-        else:
-            characters = character_query.filter(Character.skill==1).fetch()
         if(pref=="strength"):
-            characters = character_query.filter(Character.strength>5).fetch()
+            characters = character_query.filter(Character.skill==int(skill)).filter(Character.color==color).filter(Character.strength>5).fetch(3)
         else:
-            characters = character_query.filter(Character.speed>5).fetch()
-        if(color=="red"):
-            characters = character_query.filter(Character.color=="red").fetch()
-        elif(color=="black"):
-            characters = character_query.filter(Character.color=="black").fetch()
-        elif(color=="orange"):
-            characters = character_query.filter(Character.color=="orange").fetch()
-        elif(color=="yellow"):
-            characters = character_query.filter(Character.color=="yellow").fetch()
-        elif(color=="green"):
-            characters = character_query.filter(Character.color=="green").fetch()
-        elif(color=="blue"):
-            characters = character_query.filter(Character.color=="blue").fetch()
-        elif(color=="pink"):
-            characters = character_query.filter(Character.color=="pink").fetch()
+            characters = character_query.filter(Character.skill==int(skill)).filter(Character.color==color).filter(Character.speed>5).fetch(3)
         prefs_template = jinja_environment.get_template('templates/prefs.html')
         character_dict = {'character':characters}
         self.response.write(prefs_template.render(character_dict))
-        # mario = Character(name="Mario",speed=8,strength=9,color="red",wiki_link="no")
 
 
 app = webapp2.WSGIApplication([
