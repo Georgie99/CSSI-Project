82352aa63418eb86ae96ba5b4c27d4461cb47f64
N'Zinga Hammonds-Wyatt <33060600+ZingaHam@users.noreply.github.com>
2018-07-23 12:22:14 -0400

    model.py
    The python file for the project. had problems submitting  sm as a dict so it's commented out. Possible Solution: Making individual var for each move combo
    
    Co-Authored-By: shruthisanthanam <shruthisanthanam@users.noreply.github.com>

---
+++ model.py
@@ -0,0 +1,11 @@
+from google.appengine.ext import ndb
+
+class character(ndb.Model):
+    name=ndb.StringProperty(required=True)
+    speed=ndb.IntegerProperty(required=True)
+    skill=ndb.IntegerProperty(required=True)
+    strength=ndb.IntegerProperty(required=True)
+    color=ndb.StringProperty(required=True)
+    wiki_link=ndb.StringProperty(required=True)
+    #sm=ndb.UserProperty(required=True)
+    #we had problems putting it as a dic thinking of making seperate properties
