fcb9a1f84323e5bdff31a00cc75cd2a31c399b9a
N'Zinga Hammonds-Wyatt <33060600+ZingaHam@users.noreply.github.com>
2018-07-26 12:29:49 -0400

    No Changes
    Just commented out lines

--- scripts/save.js
+++ scripts/save.js
@@ -1,9 +1,11 @@
 var buttons = document.querySelectorAll(".savebutton");
+// let characterdiv = document.querySelectorAll("#character");
 if(buttons){
   buttons.forEach(function(button){
     button.addEventListener('click', function(){
       fetch('/prefs?type=addChar&characterKey='+this.value+'&userId='+this.getAttribute('user_id'),{method:"post"});
       console.log(this.getAttribute('user_id'));
+      // characterdiv.classList.toggle("tada fast");
     })
   }
   )
